9092a870596e0f8b7d68dea76be991afb0d4ec4f
Svyatoslav <chel_c@mail.ru>
2020-10-11 00:32:30 +0300

    less_2

---
+++ L2/src/gb_l2_hw/Main.java
@@ -0,0 +1,13 @@
+package gb_l2_hw;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Введите операцию");
+        System.out.println("1. Сложение");
+        System.out.println("2. Вычитание");
+        System.out.println("3. Умножение");
+    }
+}
