adf28aba8170a392147c4853b5debe0d876419ef
Svyatoslav <chel_c@mail.ru>
2020-10-10 18:54:16 +0300

    less_1

--- L1/src/gb_l1_hw/Main.java
+++ L1/src/gb_l1_hw/Main.java
@@ -3,6 +3,6 @@ package gb_l1_hw;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Дратути!!!");
     }
 }
