507b360d99c8f422baa4e761d715d6b0d0c6f6ad
Svyatoslav <chel_c@mail.ru>
2020-10-13 21:44:32 +0300

    less_2

--- L2/src/gb_l2_hw/Main.java
+++ L2/src/gb_l2_hw/Main.java
@@ -9,5 +9,20 @@ public class Main {
         System.out.println("1. Сложение");
         System.out.println("2. Вычитание");
         System.out.println("3. Умножение");
+        Scanner scanner = new Scanner(System.in);
+        int operation = scanner.nextInt();
+        System.out.println("Введите первое число");
+        int a = scanner.nextInt();
+        System.out.println("Введите второе число");
+        int b = scanner.nextInt();
+        int result;
+        if (operation == 1) {
+            result = a + b;
+        } else if (operation == 2) {
+            result = a - b;
+        } else {
+            result = a * b;
+        }
+        System.out.println("Результат = " + result);
     }
 }
