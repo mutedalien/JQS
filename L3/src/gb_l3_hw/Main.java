1fa78f5caea19eadeeb0e32b93cac9a65ef68f7e
Svyatoslav <chel_c@mail.ru>
2020-10-16 22:43:37 +0300

    less_3

--- L3/src/gb_l3_hw/Main.java
+++ L3/src/gb_l3_hw/Main.java
@@ -1,8 +1,26 @@
 package gb_l3_hw;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ваша задача угадать число");
+        int range = 10;
+        int number = (int) (Math.random() * range);
+        while(true) {
+            System.out.println("Угадайте число от 0 до " + range);
+            int input_number = scanner.nextInt();
+            if (input_number == number) {
+                System.out.println("Вы угадали!");
+                break;
+            } else if (input_number > number) {
+                System.out.println("Загаданное число меньше!");
+            } else {
+                System.out.println("Загаданное число больше!");
+            }
+        }
+        scanner.close();
     }
 }
