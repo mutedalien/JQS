aa16560566a8f509c24140f854b5c31cbfb071ae
Svyatoslav <chel_c@mail.ru>
2020-10-17 22:41:45 +0300

    less_5

--- L5/src/gb_l5_hw/Main.java
+++ L5/src/gb_l5_hw/Main.java
@@ -8,14 +8,13 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Ваша задача угадать число");
-        for (int i = 10; i <= 30; i += 10) {
-            int number = (int) (Math.random() * i);
-            playLevel(i, number);
-        }
+        for (int i = 10; i <= 30; i += 10) playLevel(i);
+        System.out.println("Вы выиграли!");
         scanner.close();
     }
 
-    private static void playLevel(int range, int number) {
+    private static void playLevel(int range) {
+        int number = (int) (Math.random() * range);
         while(true) {
             System.out.println("Угадайте число от 0 до " + range);
             int input_number = scanner.nextInt();
