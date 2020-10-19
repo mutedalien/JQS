d4b548f7342419975c7c846910076a3dcba5d0b2
Svyatoslav <chel_c@mail.ru>
2020-10-19 18:04:14 -0300

    less_6

--- L6/src/gb_l6_hw/GameWindow.java
+++ L6/src/gb_l6_hw/GameWindow.java
@@ -1,8 +1,17 @@
 package gb_l6_hw;
 
-public class Main {
+import javax.swing.*;
+
+public class GameWindow extends JFrame {
+
+    private static GameWindow game_window;
 
     public static void main(String[] args) {
-	// write your code here
+        game_window = new GameWindow();
+	    game_window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+	    game_window.setLocation(200, 100);
+	    game_window.setSize(906, 478);
+	    game_window.setResizable(false);
+	    game_window.setVisible(true);
     }
 }
