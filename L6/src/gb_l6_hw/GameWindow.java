4075ebaf15dcfb1f87f23b8a8918e8ba6f82bfa5
Svyatoslav <chel_c@mail.ru>
2020-10-21 18:32:37 +0300

    less_7

--- L6/src/gb_l6_hw/GameWindow.java
+++ L6/src/gb_l6_hw/GameWindow.java
@@ -1,13 +1,23 @@
 package gb_l6_hw;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.io.IOException;
 
 public class GameWindow extends JFrame {
 
     private static GameWindow game_window;
+    private static Image background;
+    private static Image game_over;
+    private static Image drop;
+    private static float drop_left = 200;
+    private static float drop_top = 200;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+    	background = ImageIO.read(GameWindow.class.getResourceAsStream("background.png"));
+		game_over = ImageIO.read(GameWindow.class.getResourceAsStream("game_over.png"));
+		drop = ImageIO.read(GameWindow.class.getResourceAsStream("drop.png"));
         game_window = new GameWindow();
 	    game_window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 	    game_window.setLocation(200, 100);
@@ -19,7 +29,9 @@ public class GameWindow extends JFrame {
     }
 
     private static void onRepaint(Graphics g) {
-    	g.fillOval(10, 10, 200, 100);
+    	g.drawImage(background, 0, 0, null);
+    	g.drawImage(drop, (int) drop_left, (int) drop_top, null);
+//    	g.drawImage(game_over, 280, 120, null);
 
 	}
 
